57cda5435f4397739e2919dffb76bc3ddfd3ea2e
Elwin <jingkangchua@gmail.com>
2022-12-04 21:13:06 +0800

    Updates

---
+++ precache-manifest.4dff5f4084f538b2249373d56218e92e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7ab558102180de7f93e",
+    "url": "/Dapp-Stake-Farming/static/css/main.e57ed92f.chunk.css"
+  },
+  {
+    "revision": "c7ab558102180de7f93e",
+    "url": "/Dapp-Stake-Farming/static/js/main.c7ab5581.chunk.js"
+  },
+  {
+    "revision": "c26464fd7221cbfa5e74",
+    "url": "/Dapp-Stake-Farming/static/js/1.c26464fd.chunk.js"
+  },
+  {
+    "revision": "236b7919b7eca7f0f4e1",
+    "url": "/Dapp-Stake-Farming/static/js/runtime~main.236b7919.js"
+  },
+  {
+    "revision": "e870843960aa6b6a2746bf0a11dac1ae",
+    "url": "/Dapp-Stake-Farming/static/media/stake.e8708439.png"
+  },
+  {
+    "revision": "cebdc53d231fa5fde30db007e512aca3",
+    "url": "/Dapp-Stake-Farming/static/media/unstake.cebdc53d.png"
+  },
+  {
+    "revision": "6b47307e0fca2d36d23198b8f5323e7e",
+    "url": "/Dapp-Stake-Farming/static/media/icon.6b47307e.png"
+  },
+  {
+    "revision": "3c74fee62fabfa1692b7ed0737e4419a",
+    "url": "/Dapp-Stake-Farming/index.html"
+  }
+];
